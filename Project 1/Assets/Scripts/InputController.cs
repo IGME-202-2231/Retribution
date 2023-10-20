23a217ce2c7e5f796b33bbc632de140079dcc9ac
Isabella Frohlich <bvf1083@g.rit.edu>
2023-10-19 22:03:11 -0400

    InputController done

---
+++ Project 1/Assets/Scripts/InputController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour
+{
+    [SerializeField]
+    MovementController movementController;
+
+    public void OnMove(InputAction.CallbackContext context) 
+    {
+        movementController.SetDirection(context.ReadValue<Vector2>());
+    }
+
+}
