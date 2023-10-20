e177ba91259de4e7e16d1911ec1170f4229c221b
Isabella Frohlich <bvf1083@g.rit.edu>
2023-10-19 22:47:19 -0400

    Checkpoint 2 done
    Player controls
    Player movement
    Player img asset

--- Project 1/Assets/Scripts/MovementController.cs
+++ Project 1/Assets/Scripts/MovementController.cs
@@ -1,18 +1,44 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+    Vector3 objectPosition = Vector3.zero;
+    [SerializeField]
+
+    float speed = 5.0f;
+    Vector3 direction = Vector3.zero;
+    Vector3 velocity = Vector3.zero;
+    private Camera gameCamera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        objectPosition = transform.position;
+        gameCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        velocity = direction * speed * Time.deltaTime;  // constant velocity despite fps
+
+        objectPosition += velocity; // adds velocity vector to position
+
+        transform.position = objectPosition;
+
+    }
+
+    public void SetDirection(Vector3 newDirection)
+    {
+        direction = newDirection.normalized;    
+
+        if (direction != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+        }
     }
 }
